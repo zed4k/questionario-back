1d560e68999aa5b527f62e3c735724114a8efd31
F2718929 Edson Zappe <z@bb.com.br>
2024-11-19 13:35:05 -0300

    first commit para estudo

---
+++ src/main/java/com/devkn8/Devkn8Application.java
@@ -0,0 +1,13 @@
+package com.devkn8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devkn8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devkn8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devkn8/Devkn8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkn8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devkn8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
